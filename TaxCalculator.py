c79850fbfcf2927bee635ace92ac28d9a8ef265c
Bryan Bijonowski <46324756+XxCrashNBurnxX@users.noreply.github.com>
2019-05-01 17:18:57 -0500

    Add files via upload

---
+++ TaxCalculator.py
@@ -0,0 +1,128 @@
+
+#Author: Bryan Bijonowski
+#Date Created: 05/01/2019
+#Date Modified: 05/01/2019
+#Purpose: To provide information regarding amount owed for taxes based on total tax liability.
+
+print("Please answer the following questions according to your filing status...\n")
+
+try:
+    
+    tax_a = float(input('What is the maximum income at 10% tax rate for your filing status?: '))
+    tax_b = float(input('What is the maximum income at 12% tax rate for your filing status?: '))
+    tax_c = float(input('What is the maximum income at 22% tax rate for your filing status?: '))
+    tax_d = float(input('What is the maximum income at 24% tax rate for your filing status?: '))
+    tax_e = float(input('What is the maximum income at 32% tax rate for your filing status?: '))
+    tax_f = float(input('What is the maximum income at 35% tax rate for your filing status?: '))
+    witholding = float(input("How much Federal Taxes are being withheld each paycheck?: "))
+    paychecks = float(input("How many paychecks do you receive in one year?: "))
+    income = float(input("What is your yearly gross income?: "))    
+    
+except ValueError:
+    print("All values supplied must be a decimal number. Exiting...")
+    exit()
+    
+
+def tax_check_a(income, tax_liability=0):
+    """Checks to see if income is greater than first tax bracket of $9,525"""
+    
+    if (income > tax_a):
+        tax_liability += (tax_a * .10)
+        cost = tax_check_b(income, tax_liability)
+        return cost
+        
+        
+    else:
+        tax_liability += (income * .10)
+        return tax_liability
+            
+        
+def tax_check_b(income, tax_liability):
+    """Checks to see if income is greater than second tax bracket of $38,700"""
+    
+    if (income > tax_b):
+        tax_liability += ((tax_b - tax_a) * .12)
+        tax_check_c(income, tax_liability)
+        cost = tax_check_c(income, tax_liability)
+        return cost
+    
+    else:
+        tax_liability += ((income - tax_a) * .12)
+        return tax_liability
+        
+def tax_check_c(income, tax_liability):
+    """Checks to see if income is greater than third tax bracket of $82,500"""
+    
+    if (income > tax_c):
+        tax_liability += ((tax_c - tax_b) * .22)
+        tax_check_d(income, tax_liability)
+        cost = tax_check_d(income, tax_liability)
+        return cost        
+        
+    else:
+        tax_liability += ((income - tax_b) * .22)
+        return tax_liability
+        
+def tax_check_d(income, tax_liability):
+    """Checks to see if income is greater than fourth tax bracket of $157,500"""
+    
+    if (income > tax_d):
+        tax_liability += ((tax_d - tax_c) * .24)
+        tax_check_e(income, tax_liability)
+        cost = tax_check_e(income, tax_liability)
+        return cost        
+        
+    else:
+        tax_liability += ((income - tax_c) * .24)
+        return tax_liability
+
+def tax_check_e(income, tax_liability):
+    """Checks to see if income greater than fifth tax bracket of $200,000"""
+    
+    if (income > tax_e):
+        tax_liability += ((tax_e - tax_d) * .32)
+        tax_check_f(income, tax_liability)
+        cost = tax_check_f(income, tax_liability)
+        return cost        
+        
+    else:
+        tax_liability += ((income - tax_e) * .32)
+        return tax_liability
+        
+def tax_check_f(income, tax_liability):
+    """Checks to see if income is less than, equal to, or greater than the sixth tax bracket of $500,000"""
+    
+    if (income <= tax_f):
+        tax_liability += ((tax_f - tax_e) * .35)
+        return tax_liability
+    else:
+        tax_liability += (((tax_f - tax_e) * .35) + ((income - tax_f) * .37))
+        return tax_liability
+        
+  
+cost = tax_check_a(income)
+total_witholding = (witholding * paychecks)
+
+
+print('\nYour yearly tax liability is ${cost}'.format(cost=cost))
+print("\nYour total witholding is ${total_witholding}".format(total_witholding=total_witholding))
+
+if cost > total_witholding:
+    diff1 = (cost - total_witholding)
+    print("\nYou will owe the Federal Government ${diff1}".format(diff1=diff1))
+else:
+    diff2 = abs(cost - total_witholding)
+    print("\n The Federal Government will issue an estimated refund in the amount of ${diff2}".format(diff2=diff2))
+    
+
+
+        
+
+
+        
+
+    
+
+
+
+    
