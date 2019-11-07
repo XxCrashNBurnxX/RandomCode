8765a36e06b05bd0e043592965763f4ee1d0a44b
Bryan Bijonowski <46324756+XxCrashNBurnxX@users.noreply.github.com>
2019-11-07 15:04:07 -0600

    Add files via upload

---
+++ ip_sorter_v2.py
@@ -0,0 +1,53 @@
+#Author: Bryan Bijonowski
+#Date Created: 20191106
+#Date Modified: 20191106
+#Purpose: Ingest a file containing IP's with a newline delimiter, and
+#sort them numerically, returning the output to stdout.
+
+###Imports###
+
+import sys
+import re
+from ipaddress import ip_address
+
+###Main Body###
+
+def main():
+    """Takes in a file of IP addresses as an argument, reads it, and sorts them numerically, returning the output to stdout"""
+    
+    try:
+   
+        if len(sys.argv) < 2 or len(sys.argv) > 2:
+            """"Checks that at least and no more than one argument is passed."""
+            
+            print("Please provide one, and only one, argument of a file containing IP Addresses!: ip_sorter.py <file_containing_ips>")
+            sys.exit()
+            
+            
+        with open(sys.argv[1], "r") as ip_file:
+            """"""
+            
+            if len(ip_file.readlines()) <= 1:
+                """Checks to ensure the file being read in has at least two items to sort"""
+                
+                print("Please provide a list of IP's containing at least two IP Addresses to sort!")
+                sys.exit()  
+            
+            ip_file.seek(0,0) #Returns pointer back to the beginning of the file.
+            
+            valid_ips = [ip.strip() for ip in ip_file.readlines() if ip_address(ip.strip())] #Ensures each element provided is a valid IP.
+                                         
+            ip_list_sorted = sorted(valid_ips, key = lambda ip: tuple(map(int, ip.split('.'))))
+            
+            print(ip_list_sorted, file=sys.stdout)
+     
+    except ValueError as E:
+        """Validates the contents of the file as a catchall"""
+        
+        print("Please provide a file containing IP Addresses ONLY.", E)
+        
+
+###Protection###
+
+if __name__ == "__main__":
+    main()
